29665baeb7422ac74036788de49f4d708857aedc
Jeevin <70149637+tniveej@users.noreply.github.com>
2025-06-19 17:18:22 +0800

    ltex working.
    have to rename the .bat file in windos to get ltex-ls-extra to work
    properly.

---
+++ lua/plugins/ltex_extra.lua
@@ -0,0 +1,15 @@
+return {
+  "barreiroleo/ltex_extra.nvim",
+  ft = { "markdown", "tex" },
+  dependencies = { "neovim/nvim-lspconfig" },
+  -- yes, you can use the opts field, just I'm showing the setup explicitly
+  config = function()
+    require("ltex_extra").setup({
+      server_opts = {
+        on_attach = function(client, bufnr)
+          -- your on_attach process
+        end,
+      },
+    })
+  end,
+}
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,3 +1,14 @@
 return {
   { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:visimp/mason-registry", -- for ltex_plus
+      },
+    },
+  },
+  require("mason-lspconfig").setup(),
 }
