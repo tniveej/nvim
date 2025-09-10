5a926331788dc5bdc2b1940c8d4fb1185f3b1449
Jeevin <70149637+tniveej@users.noreply.github.com>
2025-09-10 15:28:19 +0800

    Update ltex_extra.lua

--- lua/plugins/ltex_extra.lua
+++ lua/plugins/ltex_extra.lua
@@ -1,16 +1,26 @@
 return {
   "barreiroleo/ltex_extra.nvim",
-  ft = { "markdown", "tex" },
+  ft = { "markdown", "tex", "text", "plaintext", "md" },
   dependencies = { "neovim/nvim-lspconfig" },
   -- yes, you can use the opts field, just I'm showing the setup explicitly
+  opts = {
+    load_langs = { "en_GB", "en_US" },
+    path = ".ltex",
+  },
   config = function()
     require("ltex_extra").setup({
       server_opts = {
         on_attach = function(client, bufnr)
-          -- your on_attach process
+          client.server_capabilities.codeActionProvider = true
+          -- Add keymaps for code actions (example using <leader>ca)
+          -- vim.keymap.set("n", "<leader>ca", function()
+          --   vim.lsp.buf.code_action()
+          -- end, { buffer = bufnr })
         end,
         settings = {
           ltex = {
+            checkFrequency = "save",
+            enabled = { "markdown", "tex", "text", "plaintext", "md", "latex" },
             language = "en_GB",
           },
         },
